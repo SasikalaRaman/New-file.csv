4079c45bf2acad09dd5d9b910bd6e0a89e534579
SasikalaRaman <107214421+SasikalaRaman@users.noreply.github.com>
2023-01-13 11:18:30 -0800

    Update new-file.csv.py

--- new-file.csv.py
+++ new-file.csv.py
@@ -19,6 +19,8 @@ df["INDUS"].sum() # sum the values in the INDUS column
 
 Rate=["TAX","CRIM","LSTAT","INDUS"] # These are the labels for pie chart
 
+Sum=[950,1678,3019,3069] 
+
 plt.pie(Sum,labels = Rate, autopct="%.2f%%") # it convert the labels  in to decimal values
 plt.legend(loc=(1,1)) # 00,11,01,10 # poisition of chart
 plt.show() # show the diagram
